5fad76559239597e3c84e8f3caef58cb6fe65823
Gustavo <gustavo.shuiti.ferreira@usp.br>
2019-09-03 17:04:38 -0300

    encode from char[]

--- src/Base64Tools/Methods.java
+++ src/Base64Tools/Methods.java
@@ -9,35 +9,70 @@ abstract class Methods {
 
         final int len = bytes.length;
         final int outLen = (len/3 + (len%3 == 0 ? 0 : 1)) * 4;
-        final byte[] outputBytes = new byte[outLen];
+        final char[] chars = new char[outLen];
 
         int i = 0;
         int j = 0;
 
         while(i < len - 2) {
-            outputBytes[j++] = valueToChar((bytes[i] >>> 2) & 0x3F);
-            outputBytes[j++] = valueToChar(((bytes[i++] << 4) & 0x30) + ((bytes[i] >>> 4) & 0x0F));
-            outputBytes[j++] = valueToChar(((bytes[i++] << 2) & 0x3C) + ((bytes[i] >>> 6) & 0x03));
-            outputBytes[j++] = valueToChar(bytes[i++] & 0x3F);
+            chars[j++] = valueToChar((bytes[i] >>> 2) & 0x003F);
+            chars[j++] = valueToChar(((bytes[i++] << 4) & 0x0030) + ((bytes[i] >>> 4) & 0x000F));
+            chars[j++] = valueToChar(((bytes[i++] << 2) & 0x003C) + ((bytes[i] >>> 6) & 0x0003));
+            chars[j++] = valueToChar(bytes[i++] & 0x003F);
         }
 
         switch (len - i) {
             case 0:
                 break;
             case 1:
-                outputBytes[j++] = valueToChar((bytes[i] >>> 2) & 0x3F);
-                outputBytes[j++] = valueToChar((bytes[i] << 4) & 0x30);
-                outputBytes[j++] = valueToChar(64);
-                outputBytes[j] = valueToChar(64);
+                chars[j++] = valueToChar((bytes[i] >>> 2) & 0x003F);
+                chars[j++] = valueToChar((bytes[i] << 4) & 0x0030);
+                chars[j++] = valueToChar(64);
+                chars[j] = valueToChar(64);
                 break;
             case 2:
-                outputBytes[j++] = valueToChar((bytes[i] >>> 2) & 0x3F);
-                outputBytes[j++] = valueToChar(((bytes[i++] << 4) & 0x30) + ((bytes[i] >>> 4) & 0x0F));
-                outputBytes[j++] = valueToChar((bytes[i] << 2) & 0x3C);
-                outputBytes[j] = valueToChar(64);
+                chars[j++] = valueToChar((bytes[i] >>> 2) & 0x003F);
+                chars[j++] = valueToChar(((bytes[i++] << 4) & 0x0030) + ((bytes[i] >>> 4) & 0x000F));
+                chars[j++] = valueToChar((bytes[i] << 2) & 0x003C);
+                chars[j] = valueToChar(64);
                 break;
         }
-        return new String(outputBytes);
+        return new String(chars);
+    }
+
+    static String encodeChars(char[] chs) {
+
+        final int len = chs.length;
+        final int outLen = (len/3 + (len%3 == 0 ? 0 : 1)) * 4;
+        final char[] chars = new char[outLen];
+
+        int i = 0;
+        int j = 0;
+
+        while(i < len - 2) {
+            chars[j++] = valueToChar((chs[i] >>> 2) & 0x003F);
+            chars[j++] = valueToChar(((chs[i++] << 4) & 0x0030) + ((chs[i] >>> 4) & 0x000F));
+            chars[j++] = valueToChar(((chs[i++] << 2) & 0x003C) + ((chs[i] >>> 6) & 0x0003));
+            chars[j++] = valueToChar(chs[i++] & 0x003F);
+        }
+
+        switch (len - i) {
+            case 0:
+                break;
+            case 1:
+                chars[j++] = valueToChar((chs[i] >>> 2) & 0x003F);
+                chars[j++] = valueToChar((chs[i] << 4) & 0x0030);
+                chars[j++] = valueToChar(64);
+                chars[j] = valueToChar(64);
+                break;
+            case 2:
+                chars[j++] = valueToChar((chs[i] >>> 2) & 0x003F);
+                chars[j++] = valueToChar(((chs[i++] << 4) & 0x0030) + ((chs[i] >>> 4) & 0x000F));
+                chars[j++] = valueToChar((chs[i] << 2) & 0x003C);
+                chars[j] = valueToChar(64);
+                break;
+        }
+        return new String(chars);
     }
 
     static String encodeBytes_UrlEncoding(byte[] bytes) {
@@ -47,26 +82,26 @@ abstract class Methods {
         int i = 0;
 
         while(i < len - 2) {
-            sb.append(Values.encoderDictionaryURL[(bytes[i] >>> 2) & 0x3F]);
-            sb.append(Values.encoderDictionaryURL[((bytes[i++] << 4) & 0x30) + ((bytes[i] >>> 4) & 0x0F)]);
-            sb.append(Values.encoderDictionaryURL[((bytes[i++] << 2) & 0x3C) + ((bytes[i] >>> 6) & 0x03)]);
-            sb.append(Values.encoderDictionaryURL[bytes[i++] & 0x3F]);
+            sb.append(encValuesUrl[(bytes[i] >>> 2) & 0x3F]);
+            sb.append(encValuesUrl[((bytes[i++] << 4) & 0x30) + ((bytes[i] >>> 4) & 0x0F)]);
+            sb.append(encValuesUrl[((bytes[i++] << 2) & 0x3C) + ((bytes[i] >>> 6) & 0x03)]);
+            sb.append(encValuesUrl[bytes[i++] & 0x3F]);
         }
 
         switch (len - i) {
             case 0:
                 break;
             case 1:
-                sb.append(Values.encoderDictionaryURL[(bytes[i] >>> 2) & 0x3F]);
-                sb.append(Values.encoderDictionaryURL[((bytes[i] << 4) & 0x30)]);
-                sb.append(Values.encoderDictionaryURL[64]);
-                sb.append(Values.encoderDictionaryURL[64]);
+                sb.append(encValuesUrl[(bytes[i] >>> 2) & 0x3F]);
+                sb.append(encValuesUrl[((bytes[i] << 4) & 0x30)]);
+                sb.append(encValuesUrl[64]);
+                sb.append(encValuesUrl[64]);
                 break;
             case 2:
-                sb.append(Values.encoderDictionaryURL[(bytes[i] >>> 2) & 0x3F]);
-                sb.append(Values.encoderDictionaryURL[((bytes[i++] << 4) & 0x30) + ((bytes[i] >>> 4) & 0x0F)]);
-                sb.append(Values.encoderDictionaryURL[((bytes[i] << 2) & 0x3C)]);
-                sb.append(Values.encoderDictionaryURL[64]);
+                sb.append(encValuesUrl[(bytes[i] >>> 2) & 0x3F]);
+                sb.append(encValuesUrl[((bytes[i++] << 4) & 0x30) + ((bytes[i] >>> 4) & 0x0F)]);
+                sb.append(encValuesUrl[((bytes[i] << 2) & 0x3C)]);
+                sb.append(encValuesUrl[64]);
                 break;
         }
         return sb.toString();
@@ -142,15 +177,15 @@ abstract class Methods {
 
                 switch (String.format("%c%c",c1,c2).toUpperCase()) {
                     case "2B":
-                        sb.append((char) Values.encoderDictionary[62]);
+                        sb.append((char) encValues[62]);
                         k+=2;
                         break;
                     case "2F":
-                        sb.append((char) Values.encoderDictionary[63]);
+                        sb.append((char) encValues[63]);
                         k+=2;
                         break;
                     case "3D":
-                        sb.append((char) Values.encoderDictionary[64]);
+                        sb.append((char) encValues[64]);
                         k+=2;
                         break;
                     default:
@@ -211,20 +246,110 @@ abstract class Methods {
         }
     }
 
-    private static byte valueToChar(int value) {
+    private static char valueToChar(int value) {
 
-        return Values.encoderDictionary[value];
+        return encValues[value];
     }
 
     private static byte charToValue(int c) throws IOException {
 
         try {
-            byte val = Values.decoderDictionary[c];
+            byte val = decValues[c];
             _assert(val != -1);
             return val;
         }
         catch (Exception e) {
             throw new IOException("Input string is not a valid Base64 string");
         }
+    }
+
+    private final static char[] encValues = new char[] {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            '0','1','2','3','4','5','6','7','8','9',
+            '+','/','='
+    };
+
+    private final static CharSequence[] encValuesUrl = new CharSequence[] {
+            "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+            "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+            "0","1","2","3","4","5","6","7","8","9",
+            "%2B","%2F","%3D"
+    };
+
+    private final static byte[] decValues = new byte[128];
+    static {
+
+        for(int i = 0; i < 128; i++) {
+            decValues[i] = -1;
+        }
+
+        decValues['A'] = 0;
+        decValues['B'] = 1;
+        decValues['C'] = 2;
+        decValues['D'] = 3;
+        decValues['E'] = 4;
+        decValues['F'] = 5;
+        decValues['G'] = 6;
+        decValues['H'] = 7;
+        decValues['I'] = 8;
+        decValues['J'] = 9;
+        decValues['K'] = 10;
+        decValues['L'] = 11;
+        decValues['M'] = 12;
+        decValues['N'] = 13;
+        decValues['O'] = 14;
+        decValues['P'] = 15;
+        decValues['Q'] = 16;
+        decValues['R'] = 17;
+        decValues['S'] = 18;
+        decValues['T'] = 19;
+        decValues['U'] = 20;
+        decValues['V'] = 21;
+        decValues['W'] = 22;
+        decValues['X'] = 23;
+        decValues['Y'] = 24;
+        decValues['Z'] = 25;
+
+        decValues['a'] = 26;
+        decValues['b'] = 27;
+        decValues['c'] = 28;
+        decValues['d'] = 29;
+        decValues['e'] = 30;
+        decValues['f'] = 31;
+        decValues['g'] = 32;
+        decValues['h'] = 33;
+        decValues['i'] = 34;
+        decValues['j'] = 35;
+        decValues['k'] = 36;
+        decValues['l'] = 37;
+        decValues['m'] = 38;
+        decValues['n'] = 39;
+        decValues['o'] = 40;
+        decValues['p'] = 41;
+        decValues['q'] = 42;
+        decValues['r'] = 43;
+        decValues['s'] = 44;
+        decValues['t'] = 45;
+        decValues['u'] = 46;
+        decValues['v'] = 47;
+        decValues['w'] = 48;
+        decValues['x'] = 49;
+        decValues['y'] = 50;
+        decValues['z'] = 51;
+
+        decValues['0'] = 52;
+        decValues['1'] = 53;
+        decValues['2'] = 54;
+        decValues['3'] = 55;
+        decValues['4'] = 56;
+        decValues['5'] = 57;
+        decValues['6'] = 58;
+        decValues['7'] = 59;
+        decValues['8'] = 60;
+        decValues['9'] = 61;
+
+        decValues['+'] = 62;
+        decValues['/'] = 63;
     }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,9 @@
 package Main;
 
+import Base64Tools.Base64Tool;
+
 import java.io.*;
+import java.util.Base64;
 
 public class Main {
 
@@ -10,26 +13,27 @@ public class Main {
         String dir2 = "C:/Users/gusta/Downloads/earth2.bmp";
 
         File f = new File(dir);
-        File f2 = new File(dir2);
 
         byte[] image = new BufferedInputStream(new FileInputStream(f)).readAllBytes();
-        byte[] image2 = new BufferedInputStream(new FileInputStream(f2)).readAllBytes();
 
-//        String s1 = Base64.getEncoder().encodeToString(image).trim();
-//        String s2 = Base64Tool.encodeBytes(image,false);
-//
-//        System.out.println(s1.length() == s2.length());
-//        System.out.println(s1.equals(s2));
-//
-//        for(int i = 0; i < s1.length(); i++) {
-//
-//            char c1 = s1.charAt(i);
-//            char c2 = s2.charAt(i);
-//
-//            if(c1 != c2) {
-//                System.out.println(String.format("Mismatch at position %08d: '%c' should be '%c'",i,c2,c1));
-//            }
-//        }
+        char[] chars = new String(image).toCharArray();
+
+        String s1 = Base64.getEncoder().encodeToString(image).trim();
+        //String s2 = Base64Tool.encodeBytes(image,false);
+        String s2 = Base64Tool.encodeChars(chars,false);
+
+        System.out.println(s1.length() == s2.length());
+        System.out.println(s1.equals(s2));
+
+        for(int i = 0; i < s1.length(); i++) {
+
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+
+            if(c1 != c2) {
+                System.out.println(String.format("Mismatch at position %08d: '%c' should be '%c'",i,c2,c1));
+            }
+        }
 //
 //        FileOutputStream outputStream = new FileOutputStream(new File("C:/Users/gusta/Downloads/earth_mesh.bmp"));
 //
